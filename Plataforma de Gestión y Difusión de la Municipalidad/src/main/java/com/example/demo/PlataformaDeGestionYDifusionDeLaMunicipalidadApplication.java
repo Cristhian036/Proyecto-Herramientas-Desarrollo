5b14af6a1396fbc6388ba642716d3eb602c8a83b
yhojanyauli <u22104140.com>
2025-04-12 13:22:12 -0500

    demo1

---
+++ Plataforma de Gestión y Difusión de la Municipalidad/src/main/java/com/example/demo/PlataformaDeGestionYDifusionDeLaMunicipalidadApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeGestionYDifusionDeLaMunicipalidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeGestionYDifusionDeLaMunicipalidadApplication.class, args);
+	}
+
+}
